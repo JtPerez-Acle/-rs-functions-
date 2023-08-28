675f4ad75a8f4ef14c76f068d760c91225ca8bd5
JtPerez-Acle <jtperez.acle@gmail.com>
2023-08-28 13:45:30 -0400

    Final -V

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+// Another way of writing a function with a return value 
+fn main() {
+    let x = plus_one(5);
+
+    println!("The value of x is: {x}");
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
+
+/* Functions with Return Values 
+fn five() -> i32 {
+    5
+}
+
+fn main() {
+    let x = five();
+
+    println!("The value of x is: {x}");
+}
+*/
+
+/* Function with a statement that has an expression inside - Statements are instructions that perform some action and do not return a value.
+Expressions evaluate to a resultant value..
+fn main() {
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {y}");
+}
+*/
+
+/* Function with two parameters
+fn main() {
+    print_labeled_measurement(5, 'h');
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
+*/
+
+/* Function with one parameter
 fn main() {
     another_function(5);
 }
@@ -5,3 +51,4 @@ fn main() {
 fn another_function(x: i32) {
     println!("The value of x is: {x}");
 }
+*/
